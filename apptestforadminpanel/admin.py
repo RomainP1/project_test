3a474abbae6b41051f2ba9957a5f6f27f9a71caf
RomainP1 <romain.porcer.pro@gmail.com>
2025-04-10 16:46:35 +0200

    maj admin

--- apptestforadminpanel/admin.py
+++ apptestforadminpanel/admin.py
@@ -5,36 +5,50 @@ from django.contrib.auth.models import Group, User, Permission
 from django.contrib.contenttypes.models import ContentType
 from .models import Author, Book, Genre
 from django.utils.translation import ngettext
+import re # regex
 
+# Création / récupération des groupes 
 
 view_group, created = Group.objects.get_or_create(name="Lecture")
 deploy_group, created = Group.objects.get_or_create(name="Deploiement")
 support_group, created = Group.objects.get_or_create(name="Support")
 
-
+# On récupère les données des modèles de l'app 
 book_content_type = ContentType.objects.get_for_model(Book)
-post_permission = Permission.objects.filter(content_type=book_content_type)
+author_content_type = ContentType.objects.get_for_model(Author)
+genre_content_type = ContentType.objects.get_for_model(Genre)
+
+# ALTERNATIVE : Si on veut tous les modèles de l'app :
+# content_type = apps.get_models()
+
+# On récupère les permissions pour chaque modèle (CRUD de chaque modèle)
+post_permission = Permission.objects.filter(content_type__in=[book_content_type, author_content_type, genre_content_type])
 
 print([perm.codename for perm in post_permission])
 # => ['add_post', 'change_post', 'delete_post', 'view_post']
 
+# Attribution des permissions 
+
 for perm in post_permission:
-	if perm.codename == "delete_post":
+	if re.search("^delete_.*$", perm.codename):
+		deploy_group.permissions.add(perm)
+	elif re.search("^change_.*$", perm.codename):
+		support_group.permissions.add(perm)
+		deploy_group.permissions.add(perm)
+	elif re.search("^add_.*$", perm.codename):
+		support_group.permissions.add(perm)
+	elif re.search("^view_.*$", perm.codename):
+		view_group.permissions.add(perm)
+		support_group.permissions.add(perm)
 		deploy_group.permissions.add(perm)
 
-	elif perm.codename == "change_post":
-		support_group.permissions.add(perm)
-		deploy_group.permissions.add(perm)
-	elif perm.codename == "add_post":
-		view_group.permissions.add(perm)
-	else:
-		view_group.permissions.add(perm)
-		support_group.permissions.add(perm)
-		deploy_group.permissions.add(perm)
+# Création d'un user
 
 #user = User.objects.create_user(username='john',
 #                                 email='jlennon@beatles.com',
 #                                 password='glass onion')
+
+# Obtention d'un user
 
 user = User.objects.get(username="john")
 user.groups.add(view_group)  # Add the user to the Lecture group
@@ -64,12 +78,71 @@ def actiondonnee(modeladmin, request, queryset):
 def seconde_action_donnee(modeladmin, request, queryset):
 	pass
 	
+
+class AdminAuthorForm(forms.ModelForm):
+
+	# On spécifie sur quel modèle on se base
+	model = Author
+	class Meta:
+		# Affichage des champs dans la page d'ajout / edit
+		fields = "__all__"
+
+class ViewerAuthorForm(forms.ModelForm):
+	model = Author
+	class Meta:
+		fields = "__all__"
+		
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+		disable_fields(self.fields, ['name', 'surname', 'age', 'tel', 'mail', 'x',])
+
+class DeployAuthorForm(forms.ModelForm):
+	model = Author
+	class Meta:
+		fields = "__all__"
+		
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+		disable_fields(self.fields, ['tel', 'mail', 'x',])
+
+class SupportAuthorForm(forms.ModelForm):
+	model = Author
+	class Meta:
+		fields = "__all__"
+		
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+		disable_fields(self.fields, ['name', 'surname', 'age',])
+
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
 
 	# Permet d'afficher ce que renvoi le __str__ sur le panel admin
 	list_display = ['__str__']
 	list_per_page = 3
+
+	# Redéfini le formulaire (ici en fonction des groupes)
+	def get_form(self, request, obj=None, **kwargs):
+
+		user_group = "" # Initialisation à vide
+
+		# Récupération du groupe du user
+		if (len(list(request.user.groups.all()))> 0 ):
+			user_group = list(request.user.groups.all())[0].__str__()
+			
+		if request.user.is_superuser:
+			kwargs['form'] = AdminAuthorForm
+		elif (user_group == "Deploiement"):
+			kwargs['form'] = DeployAuthorForm
+		elif (user_group == "Support"):
+			kwargs['form'] = SupportAuthorForm
+		else :
+			kwargs['form'] = ViewerAuthorForm			
+		return super().get_form(request, obj, **kwargs)
 
 	# Crée des sections de champs (pour add et edit)
 	fieldsets = [
@@ -110,14 +183,15 @@ class AuthorAdmin(admin.ModelAdmin):
 		queryset |= self.model.objects.filter(name=search_term)
 		return queryset, may_have_duplicates
 
+
 # Permet de gérer les formulaires (add et edit) // Encore à voir
 class AdminBookForm(forms.ModelForm):
 	model = Book
 	class Meta:
+		# Indique qu'on veut tous les champs du modèle
 		fields = "__all__"
 
-# Permet de gérer les formulaires (add et edit) // Encore à voir
-class BookForm(forms.ModelForm):
+class ViewerBookForm(forms.ModelForm):
 	model = Book
 	class Meta:
 		# Affichage des champs dans la page d'ajout / edit
@@ -126,13 +200,26 @@ class BookForm(forms.ModelForm):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		# Liste des champs que tu veux rendre non modifiables
-		champs_ineditables = ['nb_selled_books', 'author']
-		champs = list(self.fields.keys())
 		
-		disable_fields(self.fields, champs_ineditables)
+		disable_fields(self.fields, ['nb_selled_books', 'author', 'title'])
+
+class DeployAndSupportBookForm(forms.ModelForm):
+	model = Book
+	class Meta:
+		# Affichage des champs dans la page d'ajout / edit
+		fields = "__all__"
+		
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		# Liste des champs que tu veux rendre non modifiables
+		disable_fields(self.fields, ['nb_selled_books', 'description'])
+
+
 		
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+
 	# Champs affichés
 	list_display = ['titre_colore', 'description', 'author', "nb_selled_books"]
 
@@ -140,10 +227,19 @@ class BookAdmin(admin.ModelAdmin):
 	actions = [actiondonnee, seconde_action_donnee]
 
 	def get_form(self, request, obj=None, **kwargs):
+
+		user_group = "" # Initialisation à vide
+
+		# Récupération du groupe du user
+		if (len(list(request.user.groups.all())) > 0 ): # On considère qu'il n'y a qu'un groupe par user
+			user_group = list(request.user.groups.all())[0].__str__()
+
 		if request.user.is_superuser:
 			kwargs['form'] = AdminBookForm
-		else:
-			kwargs['form'] = BookForm		
+		elif (user_group == "Deploiement" or user_group == "Support"):
+			kwargs['form'] = DeployAndSupportBookForm
+		else :
+			kwargs['form'] = ViewerBookForm			
 		return super().get_form(request, obj, **kwargs)
 
 @admin.register(Genre)
@@ -195,8 +291,9 @@ def disable_fields(form_fields, uneditable_fields):
 
 	form_fields_keys = list(form_fields.keys())
 
-	print(uneditable_fields)
 	for	champ in uneditable_fields:
 		if champ in form_fields_keys:
 			form_fields[champ].disabled = True
+			form_fields[champ].widget.attrs.update({ 'style': 'background-color:#eee !important; color:#888 !important'})
+
 ##
